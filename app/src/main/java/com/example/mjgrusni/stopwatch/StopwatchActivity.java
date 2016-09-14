75674bb3802d922cfb9513a2ba4c3fda765ec7ef
mjgrusni <mjgrusni@svsu.edu>
2016-09-14 15:16:39 -0400

    Initial Stopwatch commit

--- app/src/main/java/com/example/mjgrusni/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/example/mjgrusni/stopwatch/StopwatchActivity.java
@@ -1,13 +1,53 @@
 package com.example.mjgrusni.stopwatch;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class StopwatchActivity extends AppCompatActivity {
+
+    private boolean running;
+    private int seconds = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable(){
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = seconds / 60;
+                int secs = seconds % 60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+
+                timeView.setText(time);
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
